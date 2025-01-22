c6d3182a8c798ba260c3710bede51d48acefc823
Krishna <seandean125@gmail.com>
2025-01-22 13:58:15 -0500

    new changes

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-import * as data from "/getData.js";
+import * as data from '/getData.js';
+import { getUserId } from './userStorage.js';
 
 data.fetchData();
 
-const tabs = document.querySelectorAll("[data-tab-target]");
-const tabContents = document.querySelectorAll(".tab-content-item");
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('.tab-content-item');
 
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    const target = document.querySelector(tab.dataset.tabTarget);
-    tabs.forEach((tab) => {
-      tab.classList.remove("active");
-    });
-    tabContents.forEach((tabContent) => {
-      tabContent.classList.remove("active");
-    });
-    tab.classList.add("active");
-    target.classList.add("active");
-  });
+const userId = getUserId();
+console.log('User ID: script.js', userId);
+
+tabs.forEach(tab => {
+	tab.addEventListener('click', () => {
+		const target = document.querySelector(tab.dataset.tabTarget);
+		tabs.forEach(tab => {
+			tab.classList.remove('active');
+		});
+		tabContents.forEach(tabContent => {
+			tabContent.classList.remove('active');
+		});
+		tab.classList.add('active');
+		target.classList.add('active');
+	});
 });
+
+if (userId) {
+	console.log('Logged-in User ID: script.js', userId);
+} else {
+	console.error('User ID not found. Please log in again.');
+	// window.location.href = '/login'; // Redirect to login if no user ID is found
+}
 
 //Parcel
 
 if (module.hot) {
-  module.hot.accept();
+	module.hot.accept();
 }
--- userAuth.js
+++ userAuth.js
@@ -1,18 +1,33 @@
-import { supabase } from "/connectClient.js";
+import { supabase } from '/connectClient.js';
 
-export const user = "2b8e9bbe-e284-430f-a265-b94fed725c3a";
+// export const user = '2b8e9bbe-e284-430f-a265-b94fed725c3a';
 
 export const signUp = async function (email, password) {
-  let { data, error } = await supabase.auth.signUp({
-    email: email,
-    password: password,
-  });
+	let { data, error } = await supabase.auth.signUp({
+		email: email,
+		password: password,
+	});
 
-  if (error) {
-    console.log(error);
-    return;
-  }
+	if (error) {
+		console.log(error);
+		return;
+	}
 
-  console.log(data);
-  return;
+	console.log(data);
+	return data;
+};
+
+export const login = async function (email, password) {
+	const { data, error } = await supabase.auth.signInWithPassword({
+		email: email,
+		password: password,
+	});
+
+	if (error) {
+		console.error('Login error:', error);
+		return null;
+	}
+
+	console.log('Login successful:', data);
+	return data;
 };
